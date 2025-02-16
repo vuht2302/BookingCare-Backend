dd93924d95c5eafe8af6c47a89e92c39c28bede9
Huy Vu <huyvu230203@gmail.com>
2025-02-16 22:30:27 +0700

    update db

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -5,9 +5,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "Huy",
+        lastName: "Vu",
+        address: "VN",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
