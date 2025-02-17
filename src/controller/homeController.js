59d7d57576433392a729840fcbd1987e9f7383f6
Huy Vu <huyvu230203@gmail.com>
2025-02-17 16:45:25 +0700

    Read-data

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -24,9 +24,20 @@ let postCRUD = async (req, res) => {
   console.log(message);
   return res.send("post CRUD form servers");
 };
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  console.log("-----------------");
+
+  console.log(data);
+  console.log("-----------------");
+  return res.render("displayCRUD.ejs", {
+    dataTable: data,
+  });
+};
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,8 @@ let initWebRoute = (app) => {
   router.get("/about", homeController.getAboutPage);
   router.get("/crud", homeController.getCRUD);
   router.post("/post-crud", homeController.postCRUD);
+  router.get("/get-crud", homeController.displayGetCRUD);
+
   return app.use("/", router);
 };
 module.exports = initWebRoute;
